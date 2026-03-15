fceb7eee6bfb82be133183d02be586fe136ba556
Marta Hebda <martahebda@interia.eu>
2026-03-15 15:11:47 +0100

    Add files

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-def main():
-    print("Hello from ipum-lab-1!")
+import os
+import argparse
+from dotenv import load_dotenv
+from settings import Settings
+
+
+def export_envs(environment: str = "dev") -> None:
+    allowed = {"dev", "test", "prod"}
+
+    if environment not in allowed:
+        raise ValueError(f"Environment must be one of {allowed}")
+
+    env_path = os.path.join("config", f".env.{environment}")
+
+    if not os.path.exists(env_path):
+        raise FileNotFoundError(f"Environment file not found: {env_path}")
+
+    load_dotenv(env_path, override=True)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Load environment variables from specified.env file."
+    )
+    parser.add_argument(
+        "--environment",
+        type=str,
+        default="dev",
+        help="The environment to load (dev, test, prod)",
+    )
+    args = parser.parse_args()
+
+    export_envs(args.environment)
+
+    settings = Settings()
+
+    print("APP_NAME: ", settings.APP_NAME)
+    print("ENVIRONMENT: ", settings.ENVIRONMENT)
